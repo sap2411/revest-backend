1ffa0dbb13c22a5e465e48997fb34d4be34212a8
sap2411 <steven.parsons.us@gmail.com>
2020-07-16 15:00:19 -0700

    authorize user creation

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :password, :first_name, :last_name, :income, :age
+  attributes :id, :email, :first_name, :last_name, :income, :age
   has_many :transactions
 end
