b853439a377a81d74fd04c8be1424f65799e367e
sap2411 <steven.parsons.us@gmail.com>
2020-07-24 10:44:45 -0700

    add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,25 @@ class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    PASSWORD_FORMAT = /\A
+    (?=.{6,})          # Must contain 6 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+
+    validates :password, 
+    presence: true, 
+    length: { in: 6..20 }, 
+    format: { with: PASSWORD_FORMAT }, 
+    on: :create 
+
+    validates :password, 
+    allow_nil: true, 
+    length: { in: 6..20 }, 
+    format: { with: PASSWORD_FORMAT }, 
+    on: :update
 
     def name
         `#{self.first_name} #{self.last_name}`
