5a8e2755d728dea50326d2efd4fb4e570dd736d9
sap2411 <steven.parsons.us@gmail.com>
2020-07-27 15:07:52 -0700

    add delete and edit for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,7 +19,31 @@ class Api::V1::UsersController < ApplicationController
         render json: { error: 'failed to create user:' }, status: :not_acceptable
       end
     end
- 
+
+    def update
+      user = User.find(params[:id])
+      user.update(user_params)
+      if user.valid?
+          render json: UserSerializer.new(user)
+      else
+          render json: {errors: user.errors.full_messages}
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      if user
+          if user.transactions.length > 0
+            user.transactions.each {|tran| tran.destroy}
+          end
+          user.budgets.each{|budget| budget.destroy}
+          user.destroy
+          render json: {deleted: UserSerializer.new(user)}    
+      else
+          render json: {errors: ["User not found"]}
+      end    
+    end
+
   private
  
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, except: [:edit, :new]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       resources :auth, only: [:create]
-      resources :transactions
+      resources :transactions, only: [:index]
       post '/plaidlogin', to: 'plaid#plaidlogin'
       post '/get-transactions', to: 'plaid#getTransactions'
       get '/transactions', to: 'transactions#index'
